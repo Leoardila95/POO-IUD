329ebdd7c7244c3fe821a0aa586068ddc4c29fc7
Leoardila95 <leo950321@outlook.com>
2025-06-13 11:32:18 -0500

    Actualizar Admin.java

--- POOIUD/taller1/src/Modelos/Admin.java
+++ POOIUD/taller1/src/Modelos/Admin.java
@@ -1,5 +1,8 @@
-package modelos;
+package Modelos;
 
-public class admin{
+public class Admin extends Empleado {
+    public  Admin (String nombres, int id, double sueldo hora, int teléfono, String email) {
+        super(nombres, id, sueldo hora, teléfono, email;)
+  }
 
 }
