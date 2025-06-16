cc7b398f67693d91dff166e4b4005e093eee1e1a
Leoardila95 <leo950321@outlook.com>
2025-06-15 20:22:11 -0500

    Delete POOIUD/taller1/src/Modelos directory

--- POOIUD/taller1/src/Modelos/Admin.java
+++
@@ -1,8 +0,0 @@
-package Modelos;
-
-public class Admin extends Empleado {
-    public  Admin (String nombres, int id, double sueldo hora, int teléfono, String email) {
-        super(nombres, id, sueldo hora, teléfono, email;)
-  }
-
-}
--- POOIUD/taller1/src/Modelos/Desarrollador.java
+++
@@ -1,7 +0,0 @@
-package Modelos;
-
-public class Desarrollador extends Empleado {
-    public Desarrollador (String nombres, int id, double sueldoHora, int telefono, String email) {
-        super(nombres, id, sueldoHora, telefono, email);
-    }
-}
